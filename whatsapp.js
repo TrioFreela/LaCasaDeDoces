0c41f0d01c669267f656f057f16134359a6ee2ad
Amanda <amanda.alexandre@outlook.com>
2022-03-16 15:55:24 -0300

    Mensagem no WhatsApp (teste)

--- whatsapp.js
+++ whatsapp.js
@@ -1,8 +1,22 @@
-// https://wa.me/whatsappphonenumber?text=urlencodedtext
-// Formato do link
+// Numero do zap de Yasmin para teste: 
+// Formato do link: https://wa.me/whatsappphonenumber?text=urlencodedtext
 
 // Pegar valores do whatsapp, nome email e mensagem
-// atribuir a variaveis
+function pegaValores() {
+    let whatscliente = 5513991394497;
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let tel = document.getElementById("whatsapp").value;
+    let mensagem = document.getElementById("mensagem").value;
+
+    // Construindo o link
+    let link = `https://wa.me/${whatscliente}?text=Olá!%20Meu%20nome%20é%20${nome}%20e%20estou%20interessado(a)%20em%20falar%20com%20você%20sobre%20${mensagem}`;
+    
+}
+
+let botaoZap = document.getElementById("botao-whatsapp");
+
+botaoZap.addEventListener("click", pegaValores);
 
 // Construir o link
 
