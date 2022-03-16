e16ddcfd680409dc431bcb2235a7f55f2d3ebfbf
Amanda <amanda.alexandre@outlook.com>
2022-03-16 15:31:32 -0300

    Criando arquivo WhatsApp

---
+++ whatsapp.js
@@ -0,0 +1,9 @@
+// https://wa.me/whatsappphonenumber?text=urlencodedtext
+// Formato do link
+
+// Pegar valores do whatsapp, nome email e mensagem
+// atribuir a variaveis
+
+// Construir o link
+
+// Redirecionar para o whatsapp
